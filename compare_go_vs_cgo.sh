f235dd17364b8e5aa1ed00a4e169bf461b0f5dac
Andrew Healey <healeycodes@gmail.com>
2025-08-05 15:54:15 +0100

    add script to compare

---
+++ compare_go_vs_cgo.sh
@@ -0,0 +1,4 @@
+python3 setup_benchmarks.py
+go build -o gosyscall previousattamps/gosyscall/main.go
+go build -o gocgo previousattamps/fastestgoversion/main.go
+hyperfine --warmup 3 --min-runs 3 './gosyscall temp/deep' './gocgo temp/deep'

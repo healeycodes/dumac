bc10df4db87e71ed7599de8a030c8fc04f74a074
Andrew Healey <healeycodes@gmail.com>
2025-08-05 16:21:33 +0100

    fix compare script

--- compare_go_vs_cgo.sh
+++ compare_go_vs_cgo.sh
@@ -1,4 +1,4 @@
-python3 setup_benchmarks.py
+python3 setup_benchmark.py
 go build -o gosyscall previousattamps/gosyscall/main.go
 go build -o gocgo previousattamps/fastestgoversion/main.go
 hyperfine --warmup 3 --min-runs 3 './gosyscall temp/deep' './gocgo temp/deep'
